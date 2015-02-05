c066a7447bbd88124545cd5148d564468f29368a
Markus Elfring <elfring@users.sourceforge.net>
2015-02-05 11:48:26 +0100

    driver core: Delete an unnecessary check before the function call "put_device"
    commit 5f0163a5ee9cc7c59751768bdfd94a73186debba upstream.
    
    The put_device() function tests whether its argument is NULL and then
    returns immediately. Thus the test around the call is not needed.
    
    This issue was detected by using the Coccinelle software.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [wt: backported only to ease next patch as suggested by Jiri]
    
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -1154,7 +1154,7 @@ done:
 	glue_dir = get_glue_dir(dev);
 	kobject_del(&dev->kobj);
  Error:
-	cleanup_glue_dir(dev, glue_dir);
+	cleanup_device_parent(dev);
 	put_device(parent);
 name_error:
 	kfree(dev->p);
