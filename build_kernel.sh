741140930ee8679eee57bb6141832b791e5b5e6d
slm <you@example.com>
2025-12-24 14:19:17 +0300

    disable override toolchain path

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-
-export CROSS_COMPILE=../../../prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+export CROSS_COMPILE=../gcc4.8/bin/arm-eabi-
 export ARCH=arm
 
 make clean && make mrproper
+clear
 make lineage_goyavewifi_defconfig
-make -j20
-
-
-
+make -j$(nproc)
